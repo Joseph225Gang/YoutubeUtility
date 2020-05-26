825dc34e41f15f637a1cbf7c584e6297409a6de5
Joseph <lai225joseph@hotmail.com>
2020-05-26 21:12:03 +0800

    youtube search

---
+++ YoutubeUtility/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using YouTubeSearch;
+using static System.Console;
+
+namespace YoutubeUtility
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Write("輸入youtube要查的關鍵字 : ");
+            string searchTxt = ReadLine();
+            int searchPage = 1;
+            VideoSearch items = new VideoSearch();
+            var videoIists = await items.GetVideos(searchTxt, searchPage);
+            foreach (var item in videoIists)
+            {
+                WriteLine("-----------");
+                WriteLine($"網址:{item.getUrl()}，作者:{item.getAuthor()}，標題是 {item.getTitle()}");
+            }
+            WriteLine("結束");
+        }
+    }
+}
